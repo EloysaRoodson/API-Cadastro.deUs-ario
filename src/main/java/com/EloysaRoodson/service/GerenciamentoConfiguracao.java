2b95bfa4ef76a341644e37f3e0a82538ff341627
Eloysa Roodson <eloysaroodson@gmail.com>
2026-03-03 14:07:19 -0300

    Criação da classe GerenciamentoConfiguracao para aula prática

---
+++ src/main/java/com/EloysaRoodson/service/GerenciamentoConfiguracao.java
@@ -0,0 +1,7 @@
+package com.EloysaRoodson.service;
+
+public class GerenciamentoConfiguracao {
+     public String mensagem() {
+        return "Classe criada para teste de Gerenciamento de Configuração";
+    }
+}
